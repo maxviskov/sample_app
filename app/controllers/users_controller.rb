3eb0df0991ece05b15e24a88994af5ec53f446bc
Ubuntu <ubuntu@ip-172-31-39-1.us-east-2.compute.internal>
2024-04-18 02:08:00 +0000

    Working Menu and Index

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,11 @@
 class UsersController < ApplicationController
-
+  before_action :logged_in_user, only: [:index, :edit, :update]
+  before_action :correct_user,   only: [:edit, :update]
+  
+  def index
+      @users = User.all
+  end  
+  
   def show
     @user = User.find(params[:id])
   end
@@ -9,16 +15,29 @@ class UsersController < ApplicationController
   end
 
   def create
-        @user = User.new(user_params)
+    @user = User.new(user_params)
     if @user.save
-      # Handle a successful save.
-     reset_session
+      reset_session
       log_in @user
       flash[:success] = "Welcome to the Sample App!"
-      
       redirect_to @user
     else
       render 'new', status: :unprocessable_entity
+    end
+  end
+
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update(user_params)
+      # Handle a successful update.
+      flash[:success] = "Profile updated"
+      redirect_to @user      
+    else
+      render 'edit', status: :unprocessable_entity
     end
   end
 
@@ -27,5 +46,23 @@ class UsersController < ApplicationController
     def user_params
       params.require(:user).permit(:name, :email, :password,
                                    :password_confirmation)
-    end  
+    end
+    
+    # Before filters
+
+    # Confirms a logged-in user.
+    def logged_in_user
+      unless logged_in?
+        store_location
+        flash[:danger] = "Please log in."
+        redirect_to login_url, status: :see_other
+      end
+    end    
+    
+    # Confirms the correct user.
+    def correct_user
+      @user = User.find(params[:id])
+      redirect_to(root_url, status: :see_other) unless @user == current_user
+      redirect_to(root_url, status: :see_other) unless current_user?(@user)
+    end    
 end
